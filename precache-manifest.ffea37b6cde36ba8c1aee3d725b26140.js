1aaeb872645987fa2d6011ade4e9018b55cfe4be
nevken7 <51195535+nevken7@users.noreply.github.com>
2019-09-20 14:07:11 +0530

    Add files via upload

---
+++ precache-manifest.ffea37b6cde36ba8c1aee3d725b26140.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5df923cd7c2bd6decf1f20b2527f6c5f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e0f2b9973c7b87dc189",
+    "url": "/static/css/main.109a5c4f.chunk.css"
+  },
+  {
+    "revision": "9995dcb45d10e5fcaa9b",
+    "url": "/static/js/2.3a1a68c2.chunk.js"
+  },
+  {
+    "revision": "8e0f2b9973c7b87dc189",
+    "url": "/static/js/main.fad8aa6b.chunk.js"
+  },
+  {
+    "revision": "cdd8e56fc1e712e9fe5e",
+    "url": "/static/js/runtime~main.8ce62bd2.js"
+  }
+]);
